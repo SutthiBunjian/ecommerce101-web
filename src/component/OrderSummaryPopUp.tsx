5a88d48c8eca04e0324c6c1d9e9e6af4919d9a9e
Sutthiporn Buniian <prdi@Sutthiporns-MacBook-Air.local>
2024-07-25 15:15:16 +0700

    update

--- src/component/OrderSummaryPopUp.tsx
+++ src/component/OrderSummaryPopUp.tsx
@@ -26,7 +26,7 @@ const OrderSummaryPopUp: React.FC<OrderSummaryPopUpProps> = ({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-      <div className="relative my-10 container p-10 w-full h-full max-w-[600px] max-h-[700px] bg-white shadow-md rounded-lg mb-6 overflow-scroll">
+      <div className="relative container p-10 w-11/12 max-w-lg h-4/5 max-h-[800px] bg-white shadow-md rounded-lg overflow-auto">
         <button
           className="absolute top-2 px-4 py-2 right-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
           onClick={onClose}
@@ -35,17 +35,17 @@ const OrderSummaryPopUp: React.FC<OrderSummaryPopUpProps> = ({
         </button>
         <div className="flex flex-col items-center mb-4">
           <BsCheckCircle className="text-green-500 text-5xl mb-2" />
-          <div className="text-lg font-semibold">THANK FOR ORDERING</div>
+          <div className="text-lg font-semibold">THANK YOU FOR ORDERING</div>
         </div>
         <div className="flex justify-between items-center">
           <div className="uppercase text-lg font-semibold">Order Summary</div>
         </div>
         <div>Order Number: {ordernumber}</div>
         <br />
-        <div className="border-t border-b">
+        <div className="flex flex-col border-t border-b">
           <br />
-          <div className="flex-1 overflow-y-auto max-h-[400px]">
-            <div className="flex flex-col gap-y-2  ">
+          <div className="flex-1 overflow-y-auto max-h-[350px]">
+            <div className="flex flex-col gap-y-2">
               {cart.map((item) => (
                 <div
                   key={item.id}
@@ -59,8 +59,8 @@ const OrderSummaryPopUp: React.FC<OrderSummaryPopUpProps> = ({
                   <div className="flex flex-col text-sm font-semibold flex-grow pl-2">
                     <span>{item.title}</span>
                   </div>
-                  <div className="flex flex-col items-end text-sm ">
-                    <span className="font-semibold ">${item.price}</span>
+                  <div className="flex flex-col items-end text-sm">
+                    <span className="font-semibold">${item.price}</span>
                     <span>Qty: {item.amount}</span>
                   </div>
                 </div>
@@ -69,7 +69,7 @@ const OrderSummaryPopUp: React.FC<OrderSummaryPopUpProps> = ({
             </div>
           </div>
         </div>
-        <div className=" uppercase font-semibold absolute right-4 bottom-2 p-8">
+        <div className="flex justify-end mt-4 uppercase font-semibold">
           <span className="">Total:</span> ${total.toFixed(2)}
           <br />
         </div>
